eea9458f1b7b845e9dcab2e5450a82722ff78a16
James Romanchuk <romanchukenator@gmail.com>
2014-04-01 15:42:38 +0000

    Starting Rails Rotten Mangoes app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RottenMangoes::Application.config.secret_key_base = '70869271e31f2a23462c6c6d9ac2a0ce7686c55deae3fa9ea8c4338e2e9770902319117a794aed94351ef938a7116946fdd48c6d5925ff3cb152977cd9125fad'
